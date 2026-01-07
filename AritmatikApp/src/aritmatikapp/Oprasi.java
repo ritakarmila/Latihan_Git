83ff009e7d28ec7abe69e373d9a1ff1791fc3f59
ritakarmila <rita.karmila@mhs.itenas.ac.id>
2026-01-07 10:07:25 +0700

    hello

---
+++ AritmatikApp/src/aritmatikapp/Oprasi.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aritmatikapp;
+
+/**
+ *
+ * @author ADVAN WORKPRO
+ */
+public class Oprasi {
+    public double add(double a, double b){
+        return a+b;
+    }
+}
